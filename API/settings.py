886deb40d3e6b75ab0ba5ad03e47d68276edffe9
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-04-27 14:20:16 -0400

    4th commit

--- API/settings.py
+++ API/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../Baseball/static'),
 )
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'Baseball')
 # Default primary key field type
